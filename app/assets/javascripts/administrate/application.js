c7850f0879333c13c1fb036709ddfdb887bee504
Matt Gardner <wilburnforce@gmail.com>
2016-05-24 12:59:20 -0400

    Includes a mini-editor for the description fields.

---
+++ app/assets/javascripts/administrate/application.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require selectize
+//= require moment
+//= require datetime_picker
+//= require_tree .
+
+var simplemde = new SimpleMDE({ element: $("#markdown-text-area")[0] });
