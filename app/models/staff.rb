8d84d34dd70b4003749fce71d9d46b84cf9092d8
Matt Gardner <wilburnforce@gmail.com>
2016-06-13 17:49:22 -0400

    Adjusting default size for avatars.

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,5 +1,5 @@
 class Staff < ActiveRecord::Base
-  has_attached_file :avatar, styles: { thumbnail: "100x100#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { thumbnail: "275x275#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   belongs_to :group
