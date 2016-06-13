75e47abff497c7eae8be24be81fdbf6c115e990c
Matt Gardner <wilburnforce@gmail.com>
2016-06-13 18:05:52 -0400

    Adding position to Tools

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613204453) do
+ActiveRecord::Schema.define(version: 20160613220448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20160613204453) do
     t.text     "tagline"
     t.string   "color"
     t.string   "icon"
+    t.integer  "position"
   end
 
 end
