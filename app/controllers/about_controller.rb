44a90da85547a4592f7b996095ad465f40041470
Matt Cloyd <cloydster@gmail.com>
2016-07-29 11:06:46 -0400

    Fix sorting with position=nil

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,8 @@
 class AboutController < ApplicationController
   def index
-    @staff = Staff.all.joins(:group).select('staffs.*, groups.title').sort_by(&:position).group_by(&:title)
+    @staff = Staff.all.joins(:group).
+      select('staffs.*, groups.title').
+      sort_by{ |s| s.position.to_i }.
+      group_by(&:title)
   end
 end
