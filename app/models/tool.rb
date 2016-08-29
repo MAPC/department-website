a039770378d0d63e306da584db2444f60ceb818b
Matt Gardner <wilburnforce@gmail.com>
2016-08-29 16:32:20 -0400

    New thumbnail size

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,5 +1,5 @@
 class Tool < ActiveRecord::Base
-  has_attached_file :screenshot, styles: { thumbnail: "300x200#", large: "800x600>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :screenshot, styles: { thumbnail: "604x453>", large: "800x600>", small_thumb: "300x200#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :screenshot, content_type: /\Aimage\/.*\Z/
 
   default_scope { order(:position) }
