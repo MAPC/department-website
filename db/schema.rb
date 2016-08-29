0652575072e85625ce95fbce16663ba3f299c0e4
Matt Cloyd <cloydster@gmail.com>
2016-08-29 16:41:25 -0400

    Update schema from migrations

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20160818193557) do
     t.string   "screenshot"
     t.string   "icon"
     t.string   "url"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "preview_file_name"
     t.string   "preview_content_type"
     t.integer  "preview_file_size"
@@ -50,17 +52,21 @@ ActiveRecord::Schema.define(version: 20160818193557) do
   end
 
   create_table "services", force: :cascade do |t|
-    t.string  "title"
-    t.text    "desc"
-    t.integer "group_id"
-    t.integer "position"
+    t.string   "title"
+    t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "group_id"
+    t.integer  "position"
   end
 
   create_table "staff", force: :cascade do |t|
-    t.string  "name"
-    t.string  "email"
-    t.text    "bio"
-    t.integer "group_id"
+    t.string   "name"
+    t.string   "email"
+    t.text     "bio"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "staffs", force: :cascade do |t|
@@ -83,6 +89,8 @@ ActiveRecord::Schema.define(version: 20160818193557) do
     t.string   "screenshot"
     t.string   "url"
     t.text     "body"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "screenshot_file_name"
     t.string   "screenshot_content_type"
     t.integer  "screenshot_file_size"
