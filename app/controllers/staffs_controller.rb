68666c068ce5ebfc5ce50b64b732d123e513bb64
Matt Gardner <wilburnforce@gmail.com>
2016-08-29 17:59:04 -0400

    Includes staff bios

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,10 +1,19 @@
 class StaffsController < ApplicationController
 
+  add_breadcrumb "Home", :root_path
+
+
   def index
     @staff = Staff.all.joins(:group).
-      select('staffs.*, groups.title').
+      select('groups.*, staffs.*').
       sort_by{ |s| s.position.to_i }.
+      sort_by{ |s| s.group.position.to_i }.
       group_by(&:title)
+  end
+
+  def show
+    add_breadcrumb "About", :about_path
+    @staff = Staff.find(params[:id])
   end
 
 end
