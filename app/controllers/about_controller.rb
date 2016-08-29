a3d1f98af8e7c38801afe8ab4e758302d5002feb
Matt Gardner <wilburnforce@gmail.com>
2016-08-29 17:18:26 -0400

    Changing sort order

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,7 @@
 class AboutController < ApplicationController
   def index
     @staff = Staff.all.joins(:group).
-      select('staffs.*, groups.title').
+      select('staffs.*, groups.*').
       sort_by{ |s| s.position.to_i }.
       group_by(&:title)
   end
