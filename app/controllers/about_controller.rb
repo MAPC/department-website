e981c72cabc5eaa9687f78d0428d55a30f6c7746
Matt Gardner <wilburnforce@gmail.com>
2016-08-29 17:21:12 -0400

    Startin show back. Reverting controller change

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,7 @@
 class AboutController < ApplicationController
   def index
     @staff = Staff.all.joins(:group).
-      select('staffs.*, groups.*').
+      select('groups.*, staffs.*').
       sort_by{ |s| s.position.to_i }.
       group_by(&:title)
   end
