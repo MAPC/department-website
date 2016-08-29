0cd22fac98f861a84a2ac68ac8cfcae3723aed2a
Matt Gardner <wilburnforce@gmail.com>
2016-08-29 16:08:34 -0400

    Adding HABMT to all models

--- db/migrate/20160829194953_add_relatable_type_to_services.rb
+++
@@ -1,6 +0,0 @@
-class AddRelatableTypeToServices < ActiveRecord::Migration
-  def change
-    add_column :services, :relatable, :integer
-    add_column :services, :relatable_type, :string
-  end
-end
--- db/migrate/20160829200151_add_related_to_others.rb
+++ db/migrate/20160829200151_add_related_to_others.rb
@@ -1,4 +1,13 @@
 class AddRelatedToOthers < ActiveRecord::Migration
   def change
+    create_table :services_tools, id: false do |t|
+      t.belongs_to :service, index: true
+      t.belongs_to :tool, index: true
+    end
+
+    create_table :datasets_services, id: false do |t|
+      t.belongs_to :service, index: true
+      t.belongs_to :dataset, index: true
+    end
   end
 end
