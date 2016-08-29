4ae5fd1d7b49911a5781c2c85c1b5440c5dd82e0
Matt Cloyd <cloydster@gmail.com>
2016-08-29 17:28:03 -0400

    Change About controller/views to Staff controller/views and route to staff#index as 'about'.

--- app/controllers/about_controller.rb
+++
@@ -1,8 +0,0 @@
-class AboutController < ApplicationController
-  def index
-    @staff = Staff.all.joins(:group).
-      select('staffs.*, groups.title').
-      sort_by{ |s| s.position.to_i }.
-      group_by(&:title)
-  end
-end
--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,2 +1,10 @@
 class StaffsController < ApplicationController
+
+  def index
+    @staff = Staff.all.joins(:group).
+      select('staffs.*, groups.title').
+      sort_by{ |s| s.position.to_i }.
+      group_by(&:title)
+  end
+
 end
