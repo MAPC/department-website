8141cfd3a36c6c82201ee1857f5793faef2c6d5a
Matt Cloyd <cloydster@gmail.com>
2016-09-06 10:18:08 -0400

    Add friendly URLs to Project, Service.
    For projects and services, set the URL parameters to be id, followed by
    title. The lookup uses ID, but it's nice to see the project name.
    
    Also, remove the sort_featured scope from Project, because it's
    redundant with the default scope.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,9 +1,14 @@
 class Service < ActiveRecord::Base
-  default_scope { order('position ASC') }
-  
+
+  default_scope { order position: :asc }
+
   belongs_to :group
   has_and_belongs_to_many :projects
   has_and_belongs_to_many :tools
   has_and_belongs_to_many :datasets
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
 
 end
